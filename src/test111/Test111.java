24ccceca5f012a4aac396ab7b0b5a901e8bf7731
Ping_Shun_Wu <user@user-PC>
2012-10-04 16:30:46 +0800

    git_test #加註了 System.out.println("123");

---
+++ src/test111/Test111.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test111;
+
+/**
+ *
+ * @author user
+ */
+public class Test111 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
